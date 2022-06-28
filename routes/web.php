13d160c7f311be331118f0023ff87c66beaf7749
Eduardo <contatoeduardorezes@gmail.com>
2022-06-28 16:05:47 -0300

    Adicionado Novas Rotas para Aluno

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,15 @@ Route::get('/', function () {
 
 // Lista todos os alunos
 Route::get('/alunos', [AlunoController::class, 'index'])->name('alunos.index');
+
+// Mostra o formulário para criar um novo aluno
+Route::get('/alunos/create', [AlunoController::class, 'create'])->name('alunos.create');
+
+// Salva um novo aluno
+Route::post('/alunos/store', [AlunoController::class, 'store'])->name('alunos.store');
+
+// Mostra um aluno específico
+Route::get('/alunos/{aluno}', [AlunoController::class, 'show'])->name('alunos.show');
+
+// Mostra o formulário para editar um aluno
+Route::get('/alunos/{aluno}/edit', [AlunoController::class, 'edit'])->name('alunos.edit');
