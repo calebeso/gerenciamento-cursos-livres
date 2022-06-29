6d3fd9a8ae7dbf1a9af2a9b0bc00a887c6d26748
Eduardo <contatoeduardorezes@gmail.com>
2022-06-29 15:13:07 -0300

    Ajustes na store function para validações

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Aluno;
+use App\Models\{Aluno, Responsavel};
+use App\Http\Requests\StoreAlunoRequest;
 
 /**
  * Controller responsavel pela manipulação de alunos.
  * @author Eduardo Rezes
+ * @version 1.0
  */
 class AlunoController extends Controller
 {
@@ -15,29 +17,26 @@ class AlunoController extends Controller
      * Pagina para listar todos os alunos
      */
     public function index(){
-        //Aluno::all();
-        //dd(Aluno::all());
-        return view('alunos.index');
+        $alunos = Aluno::all();
+        //dd($alunos);
+        return view('alunos.index', compact('alunos'));
+        //return view('alunos.index');
     }
 
     /**
-     * Função para criar um novo aluno.
+     * Função que mostra o formulário para criar um novo aluno
      */
-    public function create(Request $request){
-        /**
-         * Instancia de Aluno para inserção no banco de dados;
-         */
-        // Aluno::create([
-        //     'nome' => $request->nome,
-        //     'data_nascimento' => $request->data_nascimento,
-        //     'matricula' => $request->matricula,
-        //     'status' => $request->status,
-        //     'telefone' => $request->telefone,
-        //     'rg' => $request->rg,
-        //     'cpf' => $request->cpf,
-        // ]);
-        //dd($request->all());
+    public function create(){
         return view('alunos.create');
+    }
+
+    /**
+     * Função inserir um novo aluno no banco.
+     * @param StoreAlunoRequest $request
+     */
+    public function store(StoreAlunoRequest $request){
+        //dd($request->all());
+        return view('alunos.index');
     }
 
     /**
