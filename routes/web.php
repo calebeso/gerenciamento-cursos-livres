0841dd12510cf5ec9f1554281d823754a5c09f8d
Eduardo <contatoeduardorezes@gmail.com>
2022-07-04 19:54:11 -0300

    refactor(routs) : Refatorando rotas

--- routes/web.php
+++ routes/web.php
@@ -31,17 +31,17 @@ Route::get('/', function () {
 // Lista todos os alunos
 Route::get('/alunos', [AlunoController::class, 'index'])->name('alunos.index');
 
-// Mostra o formulário para criar um novo aluno
-Route::get('/alunos/create', [AlunoController::class, 'create'])->name('alunos.create');
+// Mostra a pagina de formulário para criação de um novo aluno
+Route::get('/aluno/cadastrar-aluno', [AlunoController::class, 'create'])->name('aluno.create');
 
 // Salva um novo aluno no banco de dados
-Route::post('/alunos/store', [AlunoController::class, 'store'])->name('alunos.store');
-
-// Mostra um aluno específico
-Route::get('/alunos/{aluno}', [AlunoController::class, 'show'])->name('alunos.show');
+Route::post('/aluno/cadastrar-aluno', [AlunoController::class, 'store'])->name('aluno.store');
 
 // Mostra o formulário para editar um aluno
-Route::get('/alunos/{aluno}/edit', [AlunoController::class, 'edit'])->name('alunos.edit');
+Route::get('/aluno/editar-aluno/{id}', [AlunoController::class, 'edit'])->name('aluno.edit');
+
+// Atualiza um aluno no banco de dados
+Route::patch('/aluno/atualizar-aluno/{id}', [AlunoController::class, 'update'])->name('aluno.update');
 
 // Deleta um aluno
-Route::delete('/alunos/{aluno}', [AlunoController::class, 'destroy'])->name('alunos.destroy');
+Route::delete('/aluno/excluir-aluno/{id}', [AlunoController::class, 'delete'])->name('aluno.delete');
