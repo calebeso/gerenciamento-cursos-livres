49606dddd8a9f0587c219649c499bf2bf5f80625
limajessica <limajessica.dpa@gmail.com>
2022-07-04 21:28:32 -0300

    acrescentando campo horário, falta máscara e validação via javascript

--- resources/views/turmas/create.blade.php
+++ resources/views/turmas/create.blade.php
@@ -7,20 +7,11 @@
     <div class="card-body">
   <form action="{{ route('turma.store') }}" method="POST">
     @csrf
-    <!--ORDEM DE EXIBIÇÃO NO INDEX:
-        MODALIDADE
-        IDIOMA
-        LIVRO
-        PROFESSOR
-        DIAS DE AULA
-        HORARIO
-        STATUS
-    -->
     <div class="form-group">
-        <label for="Modalidade">Modalidade</label>
-        <input type="radio" name="modalidade" class="form-control" id="modalidade" value="connections">
+        <b><label for="Modalidade">Modalidade</label><br></b>
+        <input type="radio" name="modalidade" id="modalidade" value="connections">
         <label for="Connections">Connections</label>
-        <input type="radio" name="modalidade" class="form-control" id="modalidade" value="interactive">
+        <input type="radio" name="modalidade" id="modalidade" value="interactive">
         <label for="Interactive">Interactive</label>
     </div>
     <!--FAZER SELECT IDIOMAS-->
@@ -29,18 +20,35 @@
         <select name="livro" class="form-control" id="livro">
             <option value="">---</option>
             @foreach($livros as $livro)
-            <option value={{ $livro->id }}>{{ $livro->nome }}({{ $livro->serie }})</value>
+            <option value="{{ $livro->id }}">{{ $livro->nome }}({{ $livro->serie }})</value>
             @endforeach
         </select>
     </div>
-
     <div class="form-group">
-        <label for="Livro">Livro</label>
-        <select name="livro" class="form-control" id="livro">
+    <label for="serie">Série</label>
+    <select name="serie" class="form-control" id="serie">
             <option value="">---</option>
-            @foreach($livros as $livro)
-            <option value={{ $livro->id }}>{{ $livro->nome }}({{ $livro->serie }})</value>
-            @endforeach
+            <option value="Tots">Tots</value>
+            <option value="Little Kids">Little Kids</value>
+            <option value="Kids">Kids</value>
+            <option value="Teens">Teens</value>
+            <option value="Adults">Adults</value>
+            <option value="Idiomas">Idiomas</value>
+            <option value="Outra">Outra</value>
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="Idioma">Idioma</label>
+        <select name="idioma" class="form-control" id="idioma">
+            <option value="">---</option>
+            <option value="Inglês">Inglês</value>
+            <option value="Espanhol">Espanhol</value>
+            <option value="Alemão">Alemão</value>
+            <option value="Francês">Francês</value>
+            <option value="Italiano">Italiano</value>
+            <option value="Chinês">Chinês</value>
+            <option value="Japonês">Japonês</value>
+            <option value="Português">Português para estrangeiros</value>
         </select>
     </div>
 
@@ -49,7 +57,7 @@
         <select name="user" class="form-control" id="user">
             <option value="">---</option>
             @foreach($users as $user)
-            <option value={{ $user->id }}>{{$user->nome}}</value>
+            <option value="{{ $user->id }}">{{$user->nome}}</value>
             @endforeach
         </select>
     </div>
@@ -58,47 +66,32 @@
         <label for="Dias da semana">Dias da semana</label>
         <fieldset>
             <div>
-                <input type="checkbox" id="segunda-feira" name="dias_semana" value="segunda-feira">
+                <input type="checkbox" id="segunda-feira" name="dias_semana" value="Segunda">
                 <label for = "Segunda-feira">Segunda-feira</label>
-                <input type="checkbox" id="terça-feira" name="dias_semana" value="terça-feira">
+                <input type="checkbox" id="terça-feira" name="dias_semana" value="Terça">
                 <label for = "Terça-feira">Terça-feira</label>
-                <input type="checkbox" id="quarta-feira" name="dias_semana" value="quarta-feira">
+                <input type="checkbox" id="quarta-feira" name="dias_semana" value="Quarta">
                 <label for = "Quarta-feira">Quarta-feira</label>
-                <input type="checkbox" id="quinta-feira" name="dias_semana" value="quinta-feira">
+                <input type="checkbox" id="quinta-feira" name="dias_semana" value="Quinta">
                 <label for = "Quinta-feira">Quinta-feira</label>
-                <input type="checkbox" id="sexta-feira" name="dias_semana" value="sexta-feira">
+                <input type="checkbox" id="sexta-feira" name="dias_semana" value="Sexta">
                 <label for = "Sexta-feira">Sexta-feira</label>
-                <input type="checkbox" id="sábado" name="dias_semana" value="sábado">
+                <input type="checkbox" id="sábado" name="dias_semana" value="Sábado">
                 <label for = "Sábado">Sábado</label>
             </div>
         </fieldset>
     </div>
-
-  <div class="form-group">
-    <label for="serie">Série</label>
-    <input type="text" name="serie" class="form-control" id="serie">
-  </div>
-	<!--OU:
-	<div class="form-group">
-   		<label for="serie">Série</label>
-		<select name="serie" class="form-control" id="serie">
-			<option value="Inglês"></option>
-			<option value="Espanhol"></option>
-			<option value="Alemão"></option>
-			<option value="Francês"></option>
-			<option value="Italiano"></option>
-			<option value="Chinês"></option>
-			<option value="Japonês"></option>
-			<option value="Português para estrangeiros"></option>
-		</select>
-	</div>
-	-->
-  <div class="form-group">
-    <label for="idioma">Idioma</label>
-    <input type="text" name="idioma" class="form-control" id="idioma">
-  </div>
+    <div class="col-md-12">
+        <label for="Horário" class="form-label">Horário</label>
+        <input type="text" name="hr_inicio" class="form-control" id="hr_inicio">
+        às
+        <input type="text" name="hr_termino" class="form-control" id="hr_termino">
+    </div>
+    <br>
   <button type="submit" class="btn btn-primary">Cadastrar</button>
 </form>
     </div>
 </div>
+@endsection
+@section('javascript')
 @endsection
