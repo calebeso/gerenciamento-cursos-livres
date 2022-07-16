ae1bfb04fdad25997b9b7676882d34ba8a9c812a
calebeso <calebeso045@gmail.com>
2022-07-16 02:33:33 -0300

    dunno

--- resources/views/livros/create.blade.php
+++ resources/views/livros/create.blade.php
@@ -9,20 +9,23 @@
     @csrf
   <div class="form-group">
     <label for="nome">Nome</label>
-    <input type="text" name="nome" class="form-control" id="nome">
+    <input type="text" name="nome" class="form-control" id="nome" value="{{ old('nome') }}">
   </div>
 
   <div class="form-group">
     <label for="serie">Série</label>
-    <input type="text" name="serie" class="form-control" id="serie">
+    <input type="text" name="serie" class="form-control" id="serie" value="{{ old('serie') }}">
   </div>
 
   <div class="form-group">
     <label for="idioma">Idioma</label>
-    <input type="text" name="idioma" class="form-control" id="idioma">
+    <input type="text" name="idioma" class="form-control" id="idioma" value="{{ old('idioma') }}">
   </div>
   <button type="submit" class="btn btn-primary">Cadastrar</button>
 </form>
     </div>
 </div>
+@endsection
+@section('javascript')
+@include('includes.toastr')
 @endsection
--- resources/views/partials/javascript.blade.php
+++ resources/views/partials/javascript.blade.php
@@ -1,7 +1,14 @@
-<script src="{{ asset('js/jquery.min.js') }}"></script>
+<!-- Jquery  -->
+<script src="{{ asset('js/jquery.js') }}"></script>
+
+<!-- Bootstrap  -->
 <script src="{{ asset('js/bootstrap/bootstrap.min.js') }}"></script>
 <script src="{{ asset('js/bootstrap/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('js/popper.js') }}"></script>
+
 <script src="{{ asset('js/main.js') }}"></script>
 <script src="{{ asset('js/sidebar/sidebars.js') }}"></script>
+
+<!-- Toastr JS  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js" integrity="sha512-lbwH47l/tPXJYG9AcFNoJaTMhGvYWhVM9YI43CT+uteTRRaiLCui8snIgyAN8XWgNjNhCqlAUdzZptso6OCoFQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 @yield('javascript')
