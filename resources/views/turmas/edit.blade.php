0d53d14ec364b048b19a82b36a9a13d04e8d3c74
limajessica <gsk4af@gmail.com>
2022-08-06 22:53:06 -0300

    Acrescentando mais alguns campos na blade de edição. Ainda falta acrescentar alguns e adequar os já presentes a RNs

--- resources/views/turmas/edit.blade.php
+++ resources/views/turmas/edit.blade.php
@@ -11,21 +11,46 @@
 </div>
 <div class="my-4">
     <h3>Dados da turma</h3>
-    <button><i class="icofont-ui-edit" id="btn_editar_turma"></i></button>
+    <a href="#">
+        <i class="icofont-ui-edit" id="btn_editar_turma" style="font-size:14px;"> Editar</i>
+    </a>
+    <!--<button><i class="icofont-ui-edit" id="btn_editar_turma"></i></button>-->
 </div>
 <div class="card">
     <div class="card-body">
         <form method="POST" action="{{ route('turma.update', $turma->id) }}">
         @method('PATCH')
         @csrf
-        <div class="form-group">
-        <b><label for="Modalidade">Modalidade</label><br></b>
-        <input type="radio" name="modalidade" id="modalidade" value="connections"
-        {{ $turma->modalidade==="connections" ? 'checked' : '' }}>
-        <label for="Connections">Connections</label>
-        <input type="radio" name="modalidade" id="modalidade" value="interactive"
-        {{ $turma->modalidade==="interactive" ? 'checked' : '' }}>>
-        <label for="Interactive">Interactive</label>
+        <div class="row">
+            <div class="form-group">
+            <div class="col-md-2">
+                <label for="Professor">Professor(a)</label>
+                <select name="user" class="form-control" id="user">
+                    <option value="">---</option>
+                    @foreach($users as $user)
+                    <option value="{{ $user->id }}" {{$turma->users->id===$user->id ? 'selected':''}}>
+                        {{ $user->name }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-md-2" id="divlivro">
+                <label for="inputLivro" class="form-label mt-4 mb-2">Livro</label>
+                <select name="livro" class="form-control" id="livro">
+                    <option value="">---</option>
+                    @foreach($livros as $livro)
+                    <option value="{{ $livro->id }}">
+                        {{ $livro->nome }} ({{ $livro->serie }})
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-md-2">
+                <label for="inputId" class="form-label mt-4 mb-2">Código</label>
+                <input type="text" name="id" class="form-control" value="{{ $turma->id }}" id="inputId" readonly>
+            </div>
+            <label for="inputModalidade" class="form-label mt-4 mb-2">Modalidade:</label> {{ $turma->modalidade }}<br>
+        </div>
     </div>
     <div class="row my-4">
         <div class="d-flex justify-content-start">
@@ -44,7 +69,10 @@
 <div class="card-body">
     <div class="my-4">
         <h3>Lista de alunos</h3>
-        <button><i class="icofont-ui-edit" id="btn_editar_lista_alunos"></i></button>
+        <a href="#">
+            <i class="icofont-ui-edit" id="btn_editar_lista_alunos" style="font-size:14px;"> Editar</i>
+        </a>
+        <!--<button><i class="icofont-ui-edit" id="btn_editar_lista_alunos"></i></button>-->
     </div>
 </div>
 <script>
