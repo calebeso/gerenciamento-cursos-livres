1d76ebe01c87f48207000a045584053d17bdc0ab
Eduardo <contatoeduardorezes@gmail.com>
2022-08-15 21:10:26 -0300

    feat(responsaveis) : Criação de templates

---
+++ resources/views/responsavel/create.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+@section('content')
+<div class="my-4">
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="{{'responsavel.index'}}">Responsaveis /</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Cadastrar</li>
+    </ol>
+  </nav>
+</div>
+
+<div class="my-4">
+  <h3>Cadastrar Responsavel</h3>
+</div>
+
+<div class="card">
+  <div class="card-body">
+    <form method="POST" action="{{ route('responsavel.store') }}">
+      @csrf
+      <div class="row">
+        <div class="col-md-12">
+          <label for="inputName" class="form-label mt-4 mb-2">Nome</label>
+          <input type="text" class="form-control @error('name') is-invalid @enderror" name="nome" id="nome">
+          @error('nome')
+          <div class="invalid-feedback">{{$message}}</div>
+          @enderror
+        </div>
+
+        <div class="col-md-6">
+          <label for="inputParentesco" class="form-label mt-4 mb-2">Parentesco</label>
+          <input type="text" class="form-control @error('parentesco') is-invalid @enderror" name="parentesco" id="parentesco">
+          @error('parentesco')
+          <div class="invalid-feedback">{{$message}}</div>
+          @enderror
+        </div>
+
+        <div class="col-md-6">
+          <label for="inputTel" class="form-label mt-4 mb-2">Telefone</label>
+          <input type="text" class="form-control @error('telefone') is-invalid @enderror" id="telefone" name="telefone">
+          @error('telefone')
+          <div class="invalid-feedback">{{$message}}</div>
+          @enderror
+        </div>
+      </div>
+    </div>
+  </div>
+
+    <div class="row my-4">
+      <div class="d-flex justify-content-start">
+        <button type="submit" class="btn btn-success me-1">
+          Salvar
+        </button>
+        <button class="btn btn-danger">
+          Cancelar
+        </button>
+      </div>
+    </div>
+</form>
+@endsection
+
+@section('javascript')
+@include('includes.toastr')
+<script>
+  $('#telefone').mask('(00) 0 0000-0000');
+</script>
+@endsection
---
+++ resources/views/responsavel/index.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+@section('content')
+<div class="row align-items-center">
+    <div class="col">
+        <div class="my-4">
+            <h3>Responsaveis</h3>
+        </div>
+    </div>
+    <div class="col">
+        <a href="{{ route('responsavel.create') }}">
+            <button class="btn btn-success float-end">Novo Responsável</button>
+        </a>
+    </div>
+</div>
+<div class="card">
+    <div class="card-body">
+        <table class="table table-striped table-hover" id="responsaveis">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Parentesco</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($responsaveis as $responsavel)
+                <tr>
+                    <td scope="row">
+                        <span class="badge badge-phil bg-dark">
+                            # {{ $responsavel->id }}
+                        </span>
+                    </td>
+                    <td>
+                        {{ $responsavel->nome }}
+                    </td>
+                    <td>
+                        {{ $responsavel->parentesco }}
+                    </td>
+                    <td>
+                        {{ $responsavel->telefone }}
+                    </td>
+                    <td>
+                        <a href="{{ route('responsavel.edit', $responsavel->id ) }}" class="edit-icon me-1">
+                            <i class="icofont-ui-edit"></i>Editar
+                        </a>
+                        <form class="d-inline-block" method="POST" action="{{ route('responsavel.delete', $responsavel->id) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button id="excluir">
+                                <a class="remove-icon"><i class='icofont-ui-delete'></i>Excluir</a>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
